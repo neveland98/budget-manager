5240edc21ed13527f2abafbb94ed38108b8c5f86
neveland98 <54957992+neveland98@users.noreply.github.com>
2021-06-16 17:38:33 -0500

    began setting up service tests

--- budget-manager/src/test/java/com/talentpath/budgetmanager/services/BudgetServiceTest.java
+++ budget-manager/src/test/java/com/talentpath/budgetmanager/services/BudgetServiceTest.java
@@ -1,10 +1,24 @@
 package com.talentpath.budgetmanager.services;
 
+import com.talentpath.budgetmanager.daos.BudgetDao;
+import com.talentpath.budgetmanager.daos.CategoryRepository;
+import com.talentpath.budgetmanager.daos.RoleRepository;
+import com.talentpath.budgetmanager.daos.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BudgetServiceTest {
+
+    @Autowired
+    BudgetDao dao;
+    @Autowired
+    UserRepository repo;
+    @Autowired
+    RoleRepository roleRepo;
+    @Autowired
+    CategoryRepository categoryRepo;
 
     @BeforeEach
     void setUp() {
